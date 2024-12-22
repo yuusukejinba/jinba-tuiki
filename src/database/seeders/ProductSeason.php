0a0e358e14b1f43a2750b84ff929a79db23b6c6d
yuusuke jinba <loopdrive@softbank.ne.jp>
2024-12-22 19:11:29 +0900

    リポジトリの変更

--- src/database/seeders/ProductSeason.php
+++ src/database/seeders/ProductSeason.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeason extends Seeder
 {
@@ -18,21 +19,21 @@ class ProductSeason extends Seeder
             'season_id' => '3',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '1', //キウイ
             'season_id' => '4',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '2', //ストロベリー
             'season_id' => '1',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
 
         $param = [
@@ -40,84 +41,84 @@ class ProductSeason extends Seeder
             'season_id' => '4',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '4', //スイカ
             'season_id' => '2',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '5', //ピーチ
             'season_id' => '2',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '6', //シャインマスカット
             'season_id' => '2',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '6', //シャインマスカット
             'season_id' => '3',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '7', //パイナップル
             'season_id' => '1',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '7', //パイナップル
             'season_id' => '2',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '8', //ブドウ
             'season_id' => '2',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '8', //ブドウ
             'season_id' => '3',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '9', //バナナ
             'season_id' => '2',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '10', //メロン
             'season_id' => '1',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
         $param = [
             'product_id' => '10', //メロン
             'season_id' => '2',
             
         ];
-        DB::table('products')->insert($param);
+        DB::table('product_seasons')->insert($param);
 
 
 
