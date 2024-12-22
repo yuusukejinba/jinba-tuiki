3a3fe7c8dd30b866b6670cd89196f5c889acfa00
yuusuke jinba <loopdrive@softbank.ne.jp>
2024-12-22 19:45:26 +0900

    リポジトリの変更

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -11,77 +11,76 @@
 </head>
 
 <body>
-  <header class="header">
-    <div class="header__inner">
-        <h1>mogitate</h1>
-    </div>
-  </header>
-
-  <main>
-    <div class="register-form__content">
-      <div class="register-form__heading">
-        <div class="register-form">
-            <h2>商品登録</h2>
+    <header class="header">
+        <div class="header__inner">
+            <h1>mogitate</h1>
         </div>
+    </header>
 
+    <main>
+        <div class="register-form__content">
+            <div class="register-form__heading">
+                <div class="register-form">
+                    <h2>商品登録</h2>
+                </div>
+            </div>
         <form class="form" action="/register" method="post" >
          @csrf 
-        <div class="register-name__content">
-            <div class="register-name__inner">
-                <div class="register-name-group">
-                    <label class="register-name__product">
-                      商品名<span class="register-form__required">必須</span>
-                    </label>
+            <div class="register-name__content">
+                <div class="register-name__inner">
+                    <div class="register-name-group">
+                        <label class="register-name__product">
+                            商品名<span class="register-form__required">必須</span>
+                        </label>
+                    </div>
+                    <div class="register-name__text-inputs">
+                        <input class="register-name__input "type="text" name="register-name" id="name" placeholder="商品名を入力"> 
+                    </div>
                 </div>
-                <div class="register-name__text-inputs">
-                    <input class="register-name__input "type="text" name="register-name" id="name" placeholder="商品名を入力"> 
+
+                <div class="register-name__inner">
+                    <div class="register-name-group">
+                        <label class="register-name__product">
+                            値段<span class="register-form__required">必須</span>
+                        </label>
+                    </div>
+
+                    <div class="register-name__text-inputs">
+                        <input class="register-name__input "type="number" name="price" id="price" placeholder="値段を入力"> 
+                    </div>
+                </div>
+
+                <div class="register-name__inner">
+                    <div class="register-name-group">
+                        <label class="register-name__product "for="register-name">
+                            商品画像<span class="register-form__required">必須</span>
+                        </label>
+                    </div>
+
+                    <div class="register-name__text-inputs">
+                        <input type="" name="image" value="ファイルを入力">
+                    </div>
+                </div>
+
+                <div class="register-name__inner">
+                    <div class="register-name-group">
+                        <label class="register-name__product">季節
+                            <span class="register-form__required">必須</span>
+                            <span class="register-form__choice">複数選択可</span>
+                        </label>
+                    </div>
+
+                    <div class="register-name__season__input">
+                        <label class="register-form__season-label">
+                            <input type="radio" name="season" id="spring">春</input>
+                            <input type="radio" name="season" id="summer">夏</input>
+                            <input type="radio" name="season" id="autumn">秋</input>
+                            <input type="radio" name="season" id="winter">冬</input>
+                        </label>
+                    </div>
                 </div>
             </div>
-
-            <div class="register-name__inner">
-                <div class="register-name-group">
-                    <label class="register-name__product">
-                        値段<span class="register-form__required">必須</span>
-                    </label>
-                </div>
-
-                <div class="register-name__text-inputs">
-                    <input class="register-name__input "type="number" name="price" id="price" placeholder="値段を入力"> 
-                </div>
-            </div>
-
-            <div class="register-name__inner">
-                <div class="register-name-group">
-                    <label class="register-name__product "for="register-name">
-                        商品画像<span class="register-form__required">必須</span>
-                    </label>
-                </div>
-
-                <div class="register-name__text-inputs">
-                    <input type="text" name="text" value="ファイルを入力">
-                </div>
-            </div>
-
-            <div class="register-name__inner">
-                <div class="register-name-group">
-                    <label class="register-name__product">季節
-                        <span class="register-form__required">必須</span>
-                        <span class="register-form__choice">複数選択可</span>
-                    </label>
-                </div>
-
-                <div class="register-name__season__input">
-                    <label class="register-form__season-label">
-                        <input type="radio" name="season" id="spring">春</input>
-                        <input type="radio" name="season" id="summer">夏</input>
-                        <input type="radio" name="season" id="autumn">秋</input>
-                        <input type="radio" name="season" id="winter">冬</input>
-                    </label>
-                </div>
-            </div>
-   
-   
-
-
-</main>
+        </form>        
+        </div>
+    </main>
   </body>
